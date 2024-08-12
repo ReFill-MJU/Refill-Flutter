1d8f71d4e6b61c8b384217224b846062a445e587
arinming <jyandysys1@naver.com>
2024-08-12 18:05:37 +0900

    [Feat] 폰트, 기본 컬러 추가 (#1)

---
+++ lib/core/theme/refill_theme_color.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+class RefillThemeColor {
+  static const Color realWhite = Color(0xFFFFFFFF);
+  static const Color white = Color(0xFFFFFBF6);
+  static const Color black = Color(0xFF000000);
+
+  static const Color gray5 = Color(0xFFF4F4F4);
+  static const Color gray10 = Color(0xFFEEEDEC);
+  static const Color gray20 = Color(0xFFDFDDDA);
+  static const Color gray30 = Color(0xFFC6C4C1);
+  static const Color gray40 = Color(0xFFAAA9A6);
+  static const Color gray50 = Color(0xFF8F8D89);
+  static const Color gray60 = Color(0xFF6A6967);
+  static const Color gray70 = Color(0xFF4E4C49);
+  static const Color gray80 = Color(0xFF363532);
+  static const Color gray90 = Color(0xFF242321);
+  static const Color gray100 = Color(0xFF1A1918);
+}
