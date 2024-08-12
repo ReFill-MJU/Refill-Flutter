34ab356604af86a1030337f93d01e1a0d7321c01
arinming <jyandysys1@naver.com>
2024-08-12 18:05:19 +0900

    [Feat] 네이버 로그인 설정 (#1)

---
+++ lib/data/repository/member_repository.dart
@@ -0,0 +1,9 @@
+import '../data_source/sign_in_service.dart';
+
+class MemberRepository {
+  static Future<String?> naverLogin(String accessToken) async {
+    print(accessToken);
+    String? response = await SignInApiService().naverSignIn(accessToken);
+    return response;
+  }
+}
